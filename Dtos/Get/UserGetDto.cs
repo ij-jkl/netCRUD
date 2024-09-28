63d084dedd99676e49fa7f675af2e616a4a83b56
Isaac <isaaz.jordan02@gmail.com>
2024-09-27 22:10:20 -0300

    ADD: UserGetDto Created

---
+++ Dtos/Get/UserGetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Crud_API.Dtos.Get
+{
+    public class UserGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+    }
+}
