699b97321162f66f4fa0cdc6b20dcf45c7b1ec50
Isaac <isaaz.jordan02@gmail.com>
2024-09-27 22:20:24 -0300

    ADD: IUserRepository and UserRepository

---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Crud_API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Crud_API.Repositories
+{
+    public class UserRepository
+    {
+    }
+}
