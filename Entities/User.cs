99a92ff8e72acf4b50e819db1361eb566d742122
Isaac <isaaz.jordan02@gmail.com>
2024-09-27 21:47:45 -0300

    ADD: User Entity Added

--- Entities/User.cs
+++ Entities/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
     }
 }
